a4f8e402d84850899e8128b6ab58f1da95afdc9b
StephDw <StephDw@outlook.fr>
2020-02-05 15:52:26 +0100

    Barcode scan

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -52,7 +52,7 @@ class ApiController extends Controller
         $sessionID = explode("<FNC1>", $data)[0];
         $userID = explode("<FNC1>", $data)[1];
 
-        $participation = Participation::where([['session_id', $sessionID], ['user_id', $userID]])->get();
+        $participation = Participation::where([['session_id', $sessionID], ['user_id', $userID]])->first();
         if (count($participation) == 1) {
             $participation->flashed_at = time();
             $participation->save();
